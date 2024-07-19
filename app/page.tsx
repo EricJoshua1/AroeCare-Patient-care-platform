f463e6792f91457745a115a89de5e08f1d7e68f7
Eric Joshua Nyato <ericnyato2@gmail.com>
2024-07-19 18:02:26 +0900

    Set up the shadcn & general styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className=" text-white text-3xl underline">AroeCare Home Page</h1>
+      <Button>Press me</Button>
     </div>
   );
 }
