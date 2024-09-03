b4e411c387118059e7cda7ce885ae5ee4f9b0886
Eric Joshua <ericnyato2@gmail.com>
2024-09-03 12:21:40 +0900

    Pass all CustomProps on the Form Field

--- components/CustonFormField.tsx
+++ components/CustonFormField.tsx
@@ -9,12 +9,22 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Control } from "react-hook-form/dist";
+import { Control } from "react-hook-form";
 import { FormFieldType } from "./forms/PatientForm";
 
 interface CustomProps {
   control: Control<any>;
   fieldType: FormFieldType;
+  name: string,
+  label?: string,
+  placeholder?: string,
+  iconSrc?: string,
+  iconAlt?: string,
+  disabled?: boolean,
+  dateFormat?: string,
+  showTimeSelect?: boolean,
+  children?: React.ReactNode,
+  renderSkeleton?: (field: any) => React.ReactNode,
 }
 
 const CustonFormField = ({ control, fieldType, name, label }: CustomProps) => {
@@ -24,7 +34,7 @@ const CustonFormField = ({ control, fieldType, name, label }: CustomProps) => {
       name={name}
       render={({ field }) => (
        <FormItem className="flex-1">
-          {fieldType !== FormFieldType.CHECKBOX && label (
+          {fieldType !== FormFieldType.CHECKBOX && label && (
             <FormLabel>{label}</FormLabel>
           )}
        </FormItem>
