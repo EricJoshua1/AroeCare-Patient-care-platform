4c1fa08595ccedccec186bc21932a7bf1176ae3e
Eric Joshua <ericnyato2@gmail.com>
2024-09-05 21:32:25 +0900

    Feat: create the register page and the RegisterForm subject to further implementations

---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import CustonFormField from "../CustonFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+}
+
+
+const  RegisterForm = () => {
+  const router = useRouter();
+   const [isLoading, setIsLoading] = useState(false)
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      const userData ={name, email, phone};
+
+      const user = await createUser(userData);
+
+      if(user) router.push(`/patients/${user.$id}/register`)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋🏾 </h1>
+          <p className="text-dark-700">Schedule your first appointment</p>
+        </section>
+
+        <CustonFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name= "name"
+          label= "Full name"
+          placeholder= "Rose Anold"
+          iconSrc="assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustonFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name= "email"
+          label= "Email"
+          placeholder= "roseanold@gmail.com"
+          iconSrc="assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustonFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name= "phone"
+          label= "Phone number"
+          placeholder= "+82 10-5087-XXXX"
+        
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
