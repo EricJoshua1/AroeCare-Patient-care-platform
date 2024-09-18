92c8b10272d18af9897548c6b85cf2f8c59361fa
Eric Joshua <ericnyato2@gmail.com>
2024-09-18 23:07:25 +0900

    feat: add NewAppointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+// import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
-const NewAppointment = () => {
+export default function NewAppointment() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex h-screen max-h-screen">
+   
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px]">
+          <Image
+            src="/assets/icons/aroe-care-logo.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
 
-export default NewAppointment
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 AroeCare
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-bc.webp"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+}
