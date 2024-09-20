dacf9af991f373d14ce35e2b1c599813851a045b
Eric Joshua <ericnyato2@gmail.com>
2024-09-20 22:48:42 +0900

    fix: delete link in div for appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -18,14 +18,11 @@ export default function NewAppointment() {
           />
 
           {/* <AppointmentForm /> */}
-          <div className="text-14-regular mt-20 flex justify-between">
+          
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 AroeCare
             </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
-          </div>
+       
         </div>
       </section>
       <Image
