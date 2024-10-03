a6557e72f2fdc4f523dc487618ae7e993b2b5a0f
Eric Joshua <ericnyato2@gmail.com>
2024-10-03 17:40:34 +0900

    Feat: implement centered LINKed logo in success page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const success = () => {
   return (
-    <div>
-      success
+    <div className='flex h-screen max-h-screen px-[5%]'>
+      <div className='success-img'>
+        <Link href='/'>
+        <Image 
+          src='/assets/icons/aroe-care-logo.svg'
+          height={1000}
+          width={1000}
+          alt='logo'
+          className='h-1- w-fit'
+        />
+        </Link>
+
+      
+      </div>
     </div>
   )
 }
