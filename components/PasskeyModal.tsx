1ba14e72ffa8f7187bcca997bf8574c3b0fecd4d
Eric Joshua <ericnyato2@gmail.com>
2024-10-04 21:39:04 +0900

    Feat: install OTP component from shadcn and implement action logic

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -12,6 +12,15 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
+
+  import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+  } from "@/components/ui/input-otp"
+  
+
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
   
@@ -19,6 +28,7 @@ import { useRouter } from 'next/navigation';
 const PasskeyModal = () => {
     const router = useRouter();
     const [open, setOpen]= useState(true);
+    const [passkey, setPasskey] = useState('')
 
     const closeModal = () => {
       setOpen(false);
@@ -29,7 +39,7 @@ const PasskeyModal = () => {
   return (
   <AlertDialog open={open} onOpenChange={setOpen} >
    
-    <AlertDialogContent className='shad-aler-dialog'>
+    <AlertDialogContent className='shad-alert-dialog'>
     <AlertDialogHeader>
       <AlertDialogTitle className='flex items-start justify-between'>
         Admin Access Verification
@@ -43,9 +53,23 @@ const PasskeyModal = () => {
         />
       </AlertDialogTitle>
       <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+        To access the admin page, please enter the passkey.
       </AlertDialogDescription>
+
+    <div>
+      <InputOTP maxLength={6} value={passkey} onChange={(value) => setPasskey
+        (value)}>
+      <InputOTPGroup className='shad-otp'>
+        <InputOTPSlot className='shad-otp-slot' index={0} />
+        <InputOTPSlot className='shad-otp-slot' index={1} />
+        <InputOTPSlot className='shad-otp-slot' index={2} />
+        <InputOTPSlot className='shad-otp-slot' index={3} />
+        <InputOTPSlot className='shad-otp-slot' index={4} />
+        <InputOTPSlot className='shad-otp-slot' index={5} />
+      </InputOTPGroup> 
+      </InputOTP>
+    </div>
+
     </AlertDialogHeader>
     <AlertDialogFooter>
       <AlertDialogCancel>Cancel</AlertDialogCancel>
