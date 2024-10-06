92f0f7e279f12f526f7fdf7a53df3c46e4c998bc
Eric Joshua <ericnyato2@gmail.com>
2024-10-06 23:59:25 +0900

    Feat: editing

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Admin = () => {
   return (
-    <div>
-      Admin
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+      <header className='admin-header'>
+        <Link href='/' className='cursor-pointer' >
+            <Image
+              src="/assets/icons/aroe-care-logo.svg"
+              height={32}
+              width={162}
+              alt="logo"
+              className="h-8 w-fit"
+              
+             />
+        </Link>
+      </header>
     </div>
   )
 }
