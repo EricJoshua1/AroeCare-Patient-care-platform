59dd88943cc29ff3eb61b3121784a14c9ff252b0
Eric Joshua <ericnyato2@gmail.com>
2024-10-09 21:38:57 +0900

    Feat: add shadcn datatable & column packages & all dependancies

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,24 @@
-import DataTable from '@/components/table/DataTable'
+import {DataTable} from '@/components/table/DataTable'
 import StatCard from '@/components/StatCard'
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import columns from '@/components/table/columns'
+import {columns} from '@/components/table/columns'
+import { Payment } from '@/components/table/columns'
+
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+    ]
+  }
 
 const Admin =  async () => {
     const appointments = await getRecentAppointmentList()
