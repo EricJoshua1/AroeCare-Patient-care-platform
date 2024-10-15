3a9dcbe9b11505b7271d48c08ffb4873ecd4001f
Eric Joshua <ericnyato2@gmail.com>
2024-10-15 22:20:45 +0900

    Fix: parse Appointment instead of Payment in the columns

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import {columns} from '@/components/table/columns'
-import { Payment } from '@/components/table/columns'
+
 
 
 
