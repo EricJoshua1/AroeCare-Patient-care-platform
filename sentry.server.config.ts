a2bee6f28c2bb6f2a2003233672c64ee09670eaa
Eric Joshua <ericnyato2@gmail.com>
2024-10-17 13:15:34 +0900

    Feat : connected the App to Sentry for issues tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b94ae1fe2c3410f5e7a2287e3ea67149@o4508132367466496.ingest.us.sentry.io/4508132377165824",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
