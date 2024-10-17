95e84f58c5c31ccd7ee3e7e1784186373966fd85
Eric Joshua <ericnyato2@gmail.com>
2024-10-17 21:04:37 +0900

    Feat: create a sendSMS function as a backend action

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -15,5 +15,5 @@ client
 
 export const databases =  new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
-export const meassaging =  new sdk.Messaging(client);
+export const messaging =  new sdk.Messaging(client);
 export const users = new sdk.Users(client);
