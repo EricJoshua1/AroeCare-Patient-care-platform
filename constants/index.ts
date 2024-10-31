835f780333ae497b007af61688005651c5d94c49
Eric Joshua <ericnyato2@gmail.com>
2024-10-31 21:20:31 +0900

    change doc

--- constants/index.ts
+++ constants/index.ts
@@ -87,6 +87,10 @@ export const Doctors = [
     image: "/assets/images/dr-lee.png",
     name: "Hannah Lee",
   },
+  {
+    image: "/assets/images/dr-lee.png",
+    name: "Hannah Lee",
+  },
 ];
 
 export const StatusIcon = {
