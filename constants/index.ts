0c2b4c5f6d6008cdb0cfc25b45f1ded1a1b27119
Eric Joshua <ericnyato2@gmail.com>
2024-11-01 20:06:38 +0900

    fix

--- constants/index.ts
+++ constants/index.ts
@@ -91,6 +91,10 @@ export const Doctors = [
     image: "/assets/images/dr-lee.png",
     name: "Hannah Lee",
   },
+  {
+    image: "/assets/images/dr-lee.png",
+    name: "Hannah Lee",
+  },
 ];
 
 export const StatusIcon = {
